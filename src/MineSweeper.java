557a34274a033a7417c1923a09c24e566df1c5b6
binhsonnguyen <son@binhsonnguyen.com>
2018-02-08 09:48:01 +0700

    refactoring for more readability

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -4,8 +4,11 @@ public class MineSweeper {
                 {"*", "."}
         };
 
-        for (int xOrdinate = 0; xOrdinate < map[0].length; xOrdinate++) {
-            if (map[0][xOrdinate].equals("*")) System.out.print("*");
+        final int MAP_WIDTH = map[0].length;
+
+        for (int xOrdinate = 0; xOrdinate < MAP_WIDTH; xOrdinate++) {
+            String curentCell = map[0][xOrdinate];
+            if (curentCell.equals("*")) System.out.print("*");
             else System.out.print(1);
         }
     }
