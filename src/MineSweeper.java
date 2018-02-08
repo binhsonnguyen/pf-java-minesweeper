5f271f2dda31cee26cdc12bc9e126625040374b2
binhsonnguyen <son@binhsonnguyen.com>
2018-02-08 09:47:15 +0700

    print report for `1x2` map

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,9 +1,12 @@
 public class MineSweeper {
     public static void main(String[] args) {
         String[][] map = {
-                {"*"}
+                {"*", "."}
         };
 
-        System.out.print("*");
+        for (int xOrdinate = 0; xOrdinate < map[0].length; xOrdinate++) {
+            if (map[0][xOrdinate].equals("*")) System.out.print("*");
+            else System.out.print(1);
+        }
     }
 }
